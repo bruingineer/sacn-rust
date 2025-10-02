243aeb6f14abc3fda9f3f85519d888b8a1463311
bruingineer <joshua.g.camarena@gmail.com>
2025-10-02 16:42:03 -0700

    remove cfg check since impl is per OS already

--- src/receive.rs
+++ src/receive.rs
@@ -1213,15 +1213,11 @@ impl SacnNetworkReceiver {
         &mut self,
         buf: &'a mut [u8; RCV_BUF_DEFAULT_SIZE],
     ) -> Result<AcnRootLayerProtocol<'a>> {
-        if cfg!(windows) {
-            let n = self.socket.read(buf)?;
-            if n > RCV_BUF_DEFAULT_SIZE {
-                bail!(TooManyBytesRead(n, RCV_BUF_DEFAULT_SIZE));
-            }
-        } else {
-            self.socket.read_exact(buf)?;
+        // use read() for the windows impl, since windows does not like using read_exact()
+        // let n = ?;
+        if let Ok(n) = self.socket.read(buf) && n > RCV_BUF_DEFAULT_SIZE {
+            bail!(TooManyBytesRead(n, RCV_BUF_DEFAULT_SIZE));
         }
-
         Ok(AcnRootLayerProtocol::parse(buf)?)
     }
 
